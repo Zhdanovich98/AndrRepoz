f67e5c33b2a824131e828b3627920ca52ed1e755
Zhdanovich98 <49921618+Zhdanovich98@users.noreply.github.com>
2019-11-01 10:18:01 +0300

    Update ActivityContact.java

--- app/src/main/java/com/example/prog_002/ActivityContact.java
+++ app/src/main/java/com/example/prog_002/ActivityContact.java
@@ -1,39 +1,165 @@
 package com.example.prog_002;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityContact extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnRedact;
-    Button btnBack;
+
+    private ImageButton btnGalery;
+
+    private final int Pick_image = 1;
+
+    private EditText editName;
+    private EditText editSurname;
+    private EditText editFilial;
+    private EditText editOtdel;
+    private EditText editPost;
+    private EditText editPhone;
+    private EditText editMail;
+
+    private static String selectedName;
+    private static String selectedSurname;
+    private static String selectedFilial;
+    private static String selectedOtdel;
+    private static String selectedPost;
+    private static String selectedPhone;
+    private static String selectedMail;
+    private static String selectedImage;
+
+
+    private String getSelectedName() {
+        return selectedName;
+    }
+
+    protected void setSelectedName(final String selectedName) {
+        this.selectedName = selectedName;
+    }
+
+    private String getSelectedSurname() {
+        return selectedSurname;
+    }
+
+    protected void setSelectedSurname(final String selectedSurname) {
+        this.selectedSurname = selectedSurname;
+    }
+
+    private String getSelectedFilial() {
+        return selectedFilial;
+    }
+
+    protected void setSelectedFilial(String selectedFilial) {
+        this.selectedFilial = selectedFilial;
+    }
+
+    private String getSelectedOtdel() {
+        return selectedOtdel;
+    }
+
+    protected void setSelectedOtdel(String selectedOtdel) {
+        this.selectedOtdel = selectedOtdel;
+    }
+
+    private String getSelectedPost() {
+        return selectedPost;
+    }
+
+    protected void setSelectedPost(String selectedPost) {
+        this.selectedPost = selectedPost;
+    }
+
+    private String getSelectedPhone() {
+        return selectedPhone;
+    }
+
+    protected void setSelectedPhone(String selectedPhone) {
+        this.selectedPhone = selectedPhone;
+    }
+
+
+    private String getSelectedMail() {
+        return selectedMail;
+    }
+
+    protected void setSelectedMail(String selectedMail) { this.selectedMail = selectedMail; }
+
+    private String getSelectedImage() {
+        return selectedImage;
+    }
+
+    protected void setSelectedImage(String selectedImage) { this.selectedImage = selectedImage; }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
-        btnRedact = (Button) findViewById(R.id.btnRedact);
-        btnRedact.setOnClickListener((View.OnClickListener) this);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        btnBack = (Button) findViewById(R.id.btnBack);
-        btnBack.setOnClickListener((View.OnClickListener) this);
+        data();
+
+        btnGalery = (ImageButton) findViewById(R.id.btnImageContact);
+        btnGalery.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    void data(){
+      //  seeImageIfRedact();
+        editName = (EditText) findViewById(R.id.textName);
+        editSurname = (EditText) findViewById(R.id.textSurname);
+        editFilial = (EditText) findViewById(R.id.textFilial);
+        editOtdel = (EditText) findViewById(R.id.textOtdel);
+        editPost = (EditText) findViewById(R.id.textPost);
+        editPhone = (EditText) findViewById(R.id.textPhone);
+        editMail = (EditText) findViewById(R.id.textMail);
+        editName.setText(getSelectedName());
+        editSurname.setText(getSelectedSurname());
+        editFilial.setText(getSelectedFilial());
+        editOtdel.setText(getSelectedOtdel());
+        editPost.setText(getSelectedPost());
+        editPhone.setText(getSelectedPhone());
+        editMail.setText(getSelectedMail());
+    }
+
+    void seeImageIfRedact() {
+        if (getSelectedImage() == null) {
+        return;
+    } else
+            btnGalery.setImageURI(Uri.parse(getSelectedImage()));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case Pick_image:
+                if(resultCode == RESULT_OK){
+                    try {
+                        btnGalery.setImageURI(Uri.parse(getSelectedImage()));
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnRedact:
-                Intent intent1 = new Intent(this, ActivityRedact.class);
-                startActivity(intent1);
-                break;
-            case R.id.btnBack:
-                Intent intentForBack = new Intent(this, ActivityContacs.class);
-                startActivity(intentForBack);
+            case R.id.btnImageContact:
+
                 break;
             default:
                 break;
